dab0712026c89a36c2700f5f11af95faffd91529
azz <4zv4l@protonmail.com>
2022-08-21 15:47:20 +0200

    get manga info up

--- jpscanvf.nim
+++ jpscanvf.nim
@@ -19,31 +19,39 @@ proc clear() =
     discard execCmd("clear")
 
 proc getDest(): string =
-  stdout.write "    Chemin vers la clef usb: "
+  stdout.write "    Chemin vers le dossier: "
   readline(stdin)
 
+# ask user for a folder to add/remove
 proc getFolder(path: string): string =
   for kind, path in walkDir(path):
     case kind:
-      of pcDir: echo "Dir: ", path
+      of pcDir: echo "    ", path
       else: discard
-  stdout.write "     Quel dossier voulez-vous créer: "
+  stdout.write "    Quel dossier voulez-vous créer: "
   readline(stdin)
 
 template showMenu(dest: string) =
   showLogo()
-  echo "    Chemin vers la clef usb: ", dest
+  echo "    Chemin vers le dossier: ", dest
   echo "    1. \e[33mChoisir un manga\e[0m"
   echo "    2. \e[32mAjouter un nouveau manga\e[0m"
   echo "    3. \e[31mSupprimer un manga\e[0m"
   echo "    4. Quitter"
 
+# ask user for the menu's choice
 proc getChoice(): uint =
   stdout.write "    Que voulez-vous faire: "
   try:
     readline(stdin).parseUInt()
   except:
     5
+
+proc getInfo(): (string, string) =
+  let
+    manga = (echo "manga   : "; readline(stdin))
+    chapi = (echo "chapitre: "; readline(stdin))
+  return (manga, chapi)
 
 proc main() =
   clear()
@@ -56,10 +64,10 @@ proc main() =
     c = getChoice()
     case(c):
       of 1:
-        let 
-          mang = getManga()
-          chap = getChap()
-          url = "https://funquizzes.fun/uploads/manga/{mang}/{chap}/$i.jpg"
+        let
+          (manga, chapi) = getInfo()
+          # download all .jpg from the folder
+          url = "https://funquizzes.fun/uploads/manga/{manga}/{chapi}/"
         var client = newHttpClient()
         client.downloadFile(url, dest&"/"&manga)
         client.close()
